b27c8e66a828098d24b55d12de5dc3851855e3c4
cindycontuliano <49683116+cindycontuliano@users.noreply.github.com>
2019-06-28 15:20:10 -0400

    update plantillas

--- plantillas/base.php
+++ plantillas/base.php
@@ -1,6 +1,3 @@
-<?php 
-    //session_start();
-?>
 
 <html>
     <head>
@@ -14,9 +11,9 @@
 
     </head> 
     <body>
-        <script src="javascript/bootstrap.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="javascript/bootstrap.min.js"></script>
+        <script src="javascript/moment.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
     </body>
 </html>
--- plantillas/header.php
+++ plantillas/header.php
@@ -1,5 +1,4 @@
-<?php 
-    //session_start();
+<?php
     include 'plantillas/base.php';
 ?>
 
@@ -28,26 +27,13 @@
         float: left;
         margin-top:1px;
     }
-
-    #ChangePasswordForm {
-        position: fixed;
-        top: 0;
-        left: 0;
-        display: none;
-        width: 500px;
-        margin-left: 30%;
-        padding-bottom: 2%;
-        background: white;
-        border: 1px solid blue;
-    }
     
  </style>
 
  <html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=gb18030"><meta name="viewport" content="width=device-width, user-scalable=no">
-    
-
-    
+    <head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+        
+    	<meta name="viewport" content="width=device-width, user-scalable=no">
     </head> 
     <body>
         <header >
@@ -76,46 +62,62 @@
                     <div class = "col-3 col-md-3">
                      
                          
-                        <div class="dropdown d-flex flex-row-reverse">
+                        <div class="dropdown d-flex flex-row-reverse" id="containerDropdown">
                          
-                                <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
-                                   <span class="icon-cog"></span>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <button class="dropdown-item" onclick ="OpenChangePassword()"> <span class="icon-cw"> </span>Cambiar Clave</button>
-                                    <button class="dropdown-item" id="logout" onclick ="Logout()"><span class="icon-user"> </span>Cerrar Sesión</button>
+                            <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
+                               <span class="icon-cog"></span>
+                            </button>
+                            
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <button class="dropdown-item" data-toggle="modal" data-target="#ChangePasswordForm"> <span class="icon-cw"> </span>Cambiar Clave</button>
+                                <button class="dropdown-item" id="logout" onclick ="Logout()"><span class="icon-user"> </span>Cerrar Sesión</button>
+                            </div>
+                                
+                        <!-- Modal -->
+                            <div class="modal fade" id="ChangePasswordForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                
+                                    <!-- Modal Header -->  
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Cambiar Clave</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                  
+                                    <!-- Modal body -->
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="oldPassword">Clave Antigua</label>
+                                                <input id="oldPassword" type="password" class="form-control" placeholder="Ingrese Clave Antigua">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="newPassword">Clave Nueva</label>
+                                                <input id="newPassword" type="password" class="form-control" placeholder="Ingrese Clave Nueva">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="confirmNewPassword">Confirmar Clave Nueva</label>
+                                                <input id="confirmNewPassword" type="password" class="form-control" placeholder="Confirme Clave Nueva">
+                                            </div>
+                                        </div>
+                                        
+                                    <!-- Modal Footer -->
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary" onclick="ChangePassword()">Cambiar Clave</button>
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                                        </div>
+                 
+                                    </div>
                                 </div>
-                        </div>   
-                                         
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            
-            <div id="ChangePasswordForm">
-                <center>
-                    <h2>Cambio de Clave</h2>
-                </center>
-                
-                <div class="internalContainer">
-        
-                    <label for="oldPassword"><b>Clave Antigua</b></label>
-                    <input type="password" id="oldPassword">
-                    
-                    <label for="newPassword"><b>Clave Nueva</b></label>
-                    <input type="password" id="newPassword">
-                    
-                    <label for="confirmNewPassword"><b>Confirmar Clave Nueva</b></label>
-                    <input type="password" id="confirmNewPassword">
-                </div>
-                
-                <center>
-                    <div>
-                        <button onclick="ChangePassword()">Aceptar</button>
-                        <button onclick="CloseChangePassword()">Cancelar</button>
-                    </div>
-                </center>
-            </div>
         </header>
     </body>
  </html>
